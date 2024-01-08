2ee341b09357fa1f7a0c099175fda39967445503
Themba Mtshelwane <Thembamm3@gmail.com>
2024-01-08 18:35:38 +0200

    Show information logic

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const buttons = document.querySelectorAll('.add-minus-icon')
+const items = document.querySelectorAll('li')
 
-buttons.forEach((button) => {
+buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
     const img = button.querySelector('img')
+    const info = items[index].querySelector('p')
 
     if (img.src.includes('icon-plus.svg')) {
       img.src = './assets/images/icon-minus.svg'
+      info.classList.remove('info-hide')
     } else {
       img.src = './assets/images/icon-plus.svg'
+      info.classList.add('info-hide')
     }
   })
 })
